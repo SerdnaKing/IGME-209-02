483ef90771c2c851f44db1d1670b5f8db99d2735
Liam Andres <serdnaking77@gmail,com>
2021-02-20 22:42:59 -0500

    PE7 pointers near complete

--- PointerPEStarterFiles/PointerPEStarterFiles/ConsoleApplication10/ConsoleApplication10/ConsoleApplication10.cpp
+++ PointerPEStarterFiles/PointerPEStarterFiles/ConsoleApplication10/ConsoleApplication10/ConsoleApplication10.cpp
@@ -49,13 +49,26 @@ int main()
 	Monster *fakeMonster = (Monster*)pOne;
 
 	// add code here
-
-
-
+	cout << "Boss" << &*boss << endl;
+	boss->xPos = 42;
+	boss->yPos = 64;
+	boss->PrintPos();
+	//print out the addresses
+	
+	//cout << boss->PrintPos() << endl;
+	cout << "pOne" <<&*pOne << endl;
+	pOne->xPos = 34;
+	pOne->yPos = 35;
+	pOne->PrintPos();
+	cout <<"fakeMonster"<< &*fakeMonster << endl;
+	fakeMonster->PrintPos();
 	delete boss;
-	delete pOne;
+	delete pOne; 
+	//part 2 answer: if this is deleted, then won't fakemonster
+	//also be deleted?? they are referencing the same address
 	//delete fakeMonster; //There's a problem being triggered with this line of code
 
+	//part 6 answer: player dtor is not listed when using the base class
 	//_getch();
     return 0;
 }
