5a72b8b45319b5d77e229419eb24d1bca08f29b1
Liam Andres <serdnaking77@gmail,com>
2021-03-24 01:10:37 -0400

    PE Complete ,code commented.

--- File_IO/File_IO/File_IO.cpp
+++ File_IO/File_IO/File_IO.cpp
@@ -10,8 +10,12 @@ int main()
     //use ofstream to write a few things to a file
 
     ofstream outFile;
+
+    //create and name the text document
     outFile.open("data.txt");
 
+    //check to make sure it is open before you write to the document
+    // if open, add your text then close before exiting the if statement
     if (outFile.is_open()) {
         outFile << "Example output\n";
         outFile << "Another line\n";
@@ -19,6 +23,33 @@ int main()
     }
     
     //use ifstream object to read the file back in
+    ifstream inFile;
+    string line;
+
+    //open the named file if it exists (we know it does so there
+    //shouldn't be any errors
+    inFile.open("data.txt", ios::binary);
+
+    //seek & get the length
+    inFile.seekg(0, ios::end);
+    int length = (int)inFile.tellg();
+
+    //seek back to the beginning
+    inFile.seekg(0, ios::beg);
+    if (inFile.is_open()) {
+
+        //make an array to hold the file
+        char* fileContents = new char[length + 1];
+
+        //read file and set null pointer
+        inFile.read(fileContents, length);
+        fileContents[length] = 0;
+
+        //print the files data into the console window
+        cout << fileContents << endl;
+        //close the file    
+        inFile.close();
+    }
 
 }
 
